5487b8243400aa01c2663364b5e92d13c1cd4e9e
kshired <shiroed1211@naver.com>
2021-05-16 14:23:58 +0900

    implementation update

---
+++ implementation/segment_tree.py
@@ -0,0 +1,51 @@
+'''
+init(arr: List, tree: List, node: int, start: int, end: int) -> int 
+
+arr : 배열
+tree : 세그먼트 트리
+node의 관할 영역 : [start,end]
+'''
+def init(arr, tree, node, start, end):
+    if start == end: # 리프 노드인 경우
+        tree[node] = arr[start]
+        return tree[node]
+    tree[node] = init(arr, tree, node*2, start,(start+end)//2) + init(arr, tree, node*2+1, (start+end)//2+1, end)
+    return tree[node] # 리프 노드가 아니면 자식들의 합을 저장
+    
+'''
+sum(arr: List, tree: List, node: int, start: int, end: int, left: int, right: int) -> int 
+
+arr : 배열
+tree : 세그먼트 트리
+node의 관할 영역 : [start,end]
+구하자고 하는 영역: [left, right]
+'''
+def sum(arr, tree, node, start, end, left, right):
+    if left > end or right < start: # 겹치는 구간이 없는 경우
+        return 0
+
+    if left <= start and end <= right: # [left, right]가 [start, end]를 완전히 포함하는 경우
+        return tree[node]
+
+    return sum(arr, tree, node*2, start, (start+end)//2, left, right) + sum(arr, tree, node*2+1, (start+end)//2+1, end, left, right)
+	    # [start, end]가 [left, right]를 완전히 포함하거나
+	    # 두 구간이 겹쳐 있는 경우
+		# 왼쪽 서브 트리와 오른쪽 서브 트리에서 다시 탐색 시작
+'''
+update(arr: List, tree: List, node: int, start: int, end: int, idx: int, diff: int) -> None
+
+arr : 배열
+tree : 세그먼트 트리
+node의 관할 영역 : [start,end]
+구하자고 하는 영역: [left, right]
+idx: 바꾸자고 하는 위치
+diff: 변하는 정도
+'''
+
+def update(arr, tree, node, start, end, idx, diff):
+    if idx < start or idx > end: # 범위를 벗어남
+        return
+    tree[node] += diff # 범위에 포함되면 diff를 더해줌.
+    if start != end: # 리프 노드가 아니면, 왼쪽 서브트리와 오른쪽 서브트리 모두 update
+        update(arr, tree, node*2, start, (start+end)//2, idx, diff)
+        update(arr, tree, node*2+1, (start+end)//2+1, end, idx, diff)
---
+++ implementation/sieve_of_erathosthenes.py
@@ -0,0 +1,60 @@
+'''
+findPrime(N:int) -> None
+O(N^2)의 시간 복잡도로 N이하의 소수를 구하는 가장 쉬운 알고리즘.
+'''
+def findPrime(N):
+    prime = []
+    for i in range(2,N+1):
+        isPrime = True
+        for j in range(2,i):
+            if i % j == 0:
+                isPrime = False
+                break
+        if isPrime:
+            prime.append(i)
+
+    for i in prime:
+        print(i)
+
+'''
+findPrimeV2(N:int) -> None
+O(NlogN)의 시간 복잡도로 N이하의 소수를 구하는 에라토스테네스의 체 알고리즘
+'''
+def findPrimeV2(N):
+    prime = []
+    isPrime = [True for _ in range(N+1)]
+    isPrime[0] = False
+    isPrime[1] = False
+
+    for i in range(2,N+1):
+        if isPrime[i]:
+            prime.append(i)
+        for j in range(i*2,N+1,i):
+            isPrime[j] = False
+    
+    for i in prime:
+        print(i)
+
+'''
+findPrimeV3(N:int) -> None
+2를 제외한 짝수를 먼저 False로 만들고,
+그 후 홀수를 세면서 안쪽 for문은 sqrt(N)까지만 체크.
+'''
+def findPrimeV3(N):
+    prime = []
+    isPrime = [True for _ in range(N+1)]
+    isPrime[0] = False
+    isPrime[1] = False
+
+    for i in range(4,N+1,2):
+        isPrime[i] = False
+    
+    for i in range(3,N+1,2):
+        if not isPrime[i]:
+            continue
+        prime.append(i)
+        for j in range(i*i,N+1,i*2):
+            isPrime[j] = False
+    
+    for i in prime:
+        print(i)
